83fe4faa276a11606df03ec85d453fd97897bc4d
Rahul <Rahul@LAPTOP-JCFQGC5M>
2023-09-03 17:56:52 +0530

    CouponAPI-Backend

--- Mango.Services.CouponAPI/Models/Coupon.cs
+++ Mango.Services.CouponAPI/Models/Coupon.cs
@@ -1,9 +1,13 @@
-﻿namespace Mango.Services.CouponAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.CouponAPI.Models
 {
     public class Coupon
     {
+        [Key]
         public int CouponId { get; set; }
-        public string CouponCodes { get; set; }
+        [Required]
+        public string CouponCode { get; set; }
         public double DiscountAmount { get; set; }
         public int MinAmount { get; set; }
     }
