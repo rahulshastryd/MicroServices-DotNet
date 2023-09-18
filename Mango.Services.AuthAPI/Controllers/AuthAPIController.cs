2522e7d95afc81026282af45c8bbb38d0d8862af
Rahul Shastry D <rahul1999.jb@gmail.com>
2023-09-18 13:56:48 +0530

    Added AssignRole Functionality

--- Mango.Services.AuthAPI/Controllers/AuthAPIController.cs
+++ Mango.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -43,5 +43,17 @@ namespace Mango.Services.AuthAPI.Controllers
             _response.Result = loginResponse;
             return Ok(_response);
         }
+        [HttpPost("AssignRole")]
+        public async Task<IActionResult> AssignRole([FromBody] RegistrationRequestDto model)
+        {
+            var assignRoleSuccessfull = await _authService.AssignRole(model.Email,model.Role.ToUpper());
+            if (!assignRoleSuccessfull)
+            {
+                _response.IsSuccess = false;
+                _response.Message = "Error encountered";
+                return BadRequest(_response);
+            }
+            return Ok(_response);
+        }
     }
 }
